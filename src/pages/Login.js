ec87405a8f8f36b0d302a6c177a0e7eadb51a091
steve-alex <steve.alex@hotmail.co.uk>
2019-12-16 08:55:28 +0000

    created Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { Route, useHistory } from 'react-router-dom'
+import { Form, Checkbox, Button } from 'semantic-ui-react'
+import API from '../adapters/api.js';
+
+
+const Login = ({setUser}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([])
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    API.login({ email, password })
+      .then(resp => {
+        setUser(resp.data.email)
+        history.push("/")
+      })
+      .catch(errors => {
+        setErrors(errors)
+      })
+  }
+
+  return (
+    <>
+      <Form
+        onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>Username</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Password' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </>
+  )
+}
+
+export default Login;
