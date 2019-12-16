fc7f04a55819a12dcc3b29188a13163bd9a34096
steve-alex <steve.alex@hotmail.co.uk>
2019-12-16 08:56:16 +0000

    Validated users JWT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Route, useHistory, Redirect } from "react-router-dom"
+import Auth from "./pages/Auth.js"
+import API from './adapters/api.js';
+import Home from './pages/Home.js';
+import paths from './paths.js';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null)
+  const history = useHistory();
+
+  useEffect(() => {
+    API.validate()
+    .then(resp => {
+      setUser(resp.user)
+      history.push(paths.HOME)
+    })
+    .catch(() => {
+      history.push(paths.LOGIN)
+    })
+  }, [])
+
+  const logout = () => {
+    API.logout();
+    setUser(false);
+    history.push(paths.LOGIN);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user && <button onClick={logout}>log out</button>}
+      <Route path="/auth">
+        <Auth setUser={setUser}/>
+      </Route>
+      {
+        user ? (
+          <>
+            <Route path="/">
+              <Home user={user}/>
+            </Route>
+          </>
+          ) : (
+            <Redirect to={'/auth/login'} />
+          )
+      }
     </div>
   );
 }
