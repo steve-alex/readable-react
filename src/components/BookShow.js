a50b021b8cb289aaaa8a9a98cdcf2a25e8cdcbfb
steve-alex <steve.alex@hotmail.co.uk>
2019-12-17 22:13:08 +0000

    Built ability to create reviews on books

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,31 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { Route } from "react-router-dom"
-import paths from '../paths.js';
-import Login from '../pages/Login.js'
 import API from '../adapters/api.js'
+import AddBookToShelfForm from '../forms/AddBookToShelfForm.js';
+import NewReviewForm from '../forms/NewReviewForm.js';
 
-const BookShow = ( props ) => {
+const BookShow = ( {match, instanceToRender, setInstanceToRender, user} ) => {
   const [book, setBook] = useState([])
+  const [userHasReviewed, setUserHasReviewed] = useState(false)
 
   useEffect(() => {
-    API.getBook(props.instanceToRender)
-      .then(res => setBook(res.book.data))
+    API.getBook(instanceToRender)
+      .then(res => setBook(res.book))
+      //Also make this check if a use has reviewed the book
   }, [])
-  
-  return (
-      <div>
-        <img src={`${book.image_url}`}></img>
-        <h1>{book.title}</h1>
-        <h2>{book.subtitle}</h2>
-        <h4>{book.authors}</h4>
-        <p>{book.description}</p>
-        <p>Average Rating: {book.google_average_rating}</p>
-        <p>Number of Ratings: {book.google_average_rating}</p>
-        <br></br>
-        <h1>Friend's Activities</h1>
-        <h1>Community Reviews</h1>
 
-      </div>
+  return (
+    <div>
+      <img src={`${book.image_url}`}></img>
+      <h1>{book.title}</h1>
+      <h2>{book.subtitle}</h2>
+      <h4>{book.authors}</h4>
+      <p>{book.description}</p>
+      <AddBookToShelfForm
+        book={book}
+        userShelves={user.shelves}/>
+      <NewReviewForm
+        bookId={book.id}
+        userId={user.id}
+        setUserHasReviewed={setUserHasReviewed}
+      />
+      <p>Average Rating: {book.google_average_rating}</p>
+      <p>Number of Ratings: {book.google_average_rating}</p>
+      <br></br>
+      {userHasReviewed &&
+        <h1>My Review</h1>
+      }
+      <h1>Friend's Activities</h1>
+      <h1>Community Reviews</h1>
+    </div>
   )
 }
 
---
+++ src/forms/NewReviewForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Form, Button, TextArea, Rating } from 'semantic-ui-react'
+import API from '../adapters/api.js'
+
+const NewReviewForm = ( {bookId, userId, setUserHasReviewed} ) => {
+  const [content, setContent] = useState("")
+  const [rating, setRating] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    API.createReview(content, rating, bookId, userId)
+      .then(setUserHasReviewed(true))
+      .catch(console.log)
+    //Something here to make sure the review has gone through
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setRating(parseInt(e.target.getAttribute("aria-posinset")))
+  }
+
+  return (
+    <Form
+      onSubmit={handleSubmit}>
+      <Form.Field>
+        <TextArea
+          placeholder="What do you think?"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}/>
+      </Form.Field>
+      <Form.Field>
+      <Rating
+        icon='star'
+        defaultRating={0}
+        maxRating={5}
+        value={rating}
+        onClick={(e) => handleClick(e)}/>
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  )
+
+}
+
+
+export default NewReviewForm;
