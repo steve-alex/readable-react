fef3fb2f972aaa50356cc993d130aaff3427f985
steve-alex <steve.alex@hotmail.co.uk>
2019-12-17 22:13:51 +0000

    Home container calls effect that gets timeline

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,10 +1,20 @@
-import React, { } from 'react';
-import { Route, Link } from "react-router-dom"
-import paths from '../paths.js';
-import Login from '../pages/Login.js'
+import React, { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar.js'
+import API from '../adapters/api.js'
 
 const HomeContainer = ( {user, logout, setInstanceToRender} ) => {
+  const [timeline, setTimeline] = useState([])
+  //The search bar here, filters the users that are in the whole database, by username, email address etc.
+  ///User.find_by(email: params[:search]) \\ User.find_by(username: params[:search])
+
+  useEffect(() => {
+    //Fetch from the the users timeline serializer
+    API.getTimeline()
+      .then(res => setTimeline(res))
+    return () => {
+    };
+  }, [])
+  
   return (
       <div>
         <h1>Readable</h1>
