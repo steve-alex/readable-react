e80d24cb9c5f81dfb0edca65fc10993499c8f175
steve-alex <steve.alex@hotmail.co.uk>
2019-12-17 15:09:45 +0000

    Created handleClick for search results

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { Route, Link } from "react-router-dom"
+import { Route, Link, Red, Redirect } from "react-router-dom"
 import paths from '../paths.js';
 import { Input, Form, Button} from 'semantic-ui-react'
 import API from '../adapters/api.js'
 import AddToShelfDropDown from '../buttons/AddToShelfDropDown.js'
 import AddToShelfForm from '../forms/AddToShelfForm.js'
 
-const SearchResult = ( {book, shelves, setToRender} ) => {
+const SearchResult = ( {book, userShelves, setInstanceToRender} ) => {
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    API.findOrCreateBook(book)
+      .then((resp) => setInstanceToRender(resp.book.data.id))
+      .then(() => setClicked(true))
+  }
 
   return (
     <div className="searchResult">
-      <h1 onClick={() => setToRender(book)}>{book.title}</h1>
+      {/* <Link
+        key={book.id}
+        to={`/books/${book.google_id}`}
+        onClick={handleClick}>
+      {book.title}
+      </Link> */}
+      <a onClick={handleClick}>{book.title}</a>
+      {clicked && 
+        <Redirect to={`/books/${book.google_id}`}/>
+      }
       <AddToShelfForm
         book={book}
-        shelves={shelves}
-        setToRender={setToRender}/>
+        userShelves={userShelves}
+      />
     </div>
   )
 }
