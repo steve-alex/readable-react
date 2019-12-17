197eb2cac1617202aa87b979c6cf2f6fed131648
steve-alex <steve.alex@hotmail.co.uk>
2019-12-17 22:14:32 +0000

    Add book to shelf form created

---
+++ src/forms/AddBookToShelfForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Form, Button, Dropdown } from 'semantic-ui-react'
+import API from '../adapters/api.js'
+
+const AddBookToShelfForm = ( {book, userShelves} ) => {
+  const [shelfId, setShelfId] = useState("")
+
+  const shelfNames = () => {
+    return userShelves.map(shelf => {
+      return {
+        key: shelf.name,
+        text: shelf.name,
+        value: shelf.id
+      }
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    API.addBookToShelf(book, shelfId)
+      .then(console.log)
+      //This could be more dynamic and display something cool when you add a book to a shelf?
+  }
+
+  const onChange = (e) => {
+    if (e.target.childNodes[0]) {
+      let selectedOption = e.target.childNodes[0].textContent
+      let selectedShelf = userShelves.filter(shelf => {
+        return shelf.name === selectedOption;
+      })
+      setShelfId(selectedShelf[0].id)
+    }
+    //This if statement gets rid of a very annoying error, look properly into what is actually causing it later
+  }
+
+  return (
+    <Form
+      onSubmit={handleSubmit}>
+      <Form.Field>
+        <Dropdown
+          placeholder="Add this book to a shelf..."
+          fluid
+          search
+          selection
+          options={shelfNames()}
+          onChange={onChange}
+        />
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  )
+}
+
+export default AddBookToShelfForm;
