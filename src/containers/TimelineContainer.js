cd3f6ffaf99749d826651a6bd5031a566e370103
steve-alex <steve.alex@hotmail.co.uk>
2019-12-18 19:44:29 +0000

    Created Timeline container

---
+++ src/containers/TimelineContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Rating } from 'semantic-ui-react'
+
+const TimelineContainer = ( {instanceToRender, setInstanceToRender, timeline} ) => {
+
+  return (
+    <>
+      {timeline.timeline_posts &&
+        timeline.timeline_posts.map(post => {
+          return (
+            <>
+              <p>{post.content}</p>
+              <Rating
+                icon="star"
+                defaultRating={post.rating}
+                maxRating={5}
+                disabled/>
+            </>
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default TimelineContainer;
