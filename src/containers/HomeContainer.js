3fdba78c80aad87a82e0e612861fa2705bf344bb
steve-alex <steve.alex@hotmail.co.uk>
2019-12-18 19:44:44 +0000

    Added timeline container in

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar.js'
+import TimelineContainer from './TimelineContainer.js'
 import API from '../adapters/api.js'
 
-const HomeContainer = ( {user, logout, setInstanceToRender} ) => {
+
+
+const HomeContainer = ( {user, logout, instanceToRender, setInstanceToRender} ) => {
   const [timeline, setTimeline] = useState([])
   //The search bar here, filters the users that are in the whole database, by username, email address etc.
   ///User.find_by(email: params[:search]) \\ User.find_by(username: params[:search])
-
   useEffect(() => {
     //Fetch from the the users timeline serializer
     API.getTimeline()
-      .then(res => setTimeline(res))
-    return () => {
-    };
+      .then(res => setTimeline(res.timeline))
   }, [])
   
   return (
-      <div>
-        <h1>Readable</h1>
-        <p></p>
-        <SearchBar />
-        <br></br>
-        <button onClick={logout}>log out</button>
+    <div>
+      <h1>Readable</h1>
+      {timeline &&
+        <TimelineContainer
+          instanceToRender={instanceToRender}
+          setInstanceToRender={setInstanceToRender}
+          timeline={timeline}
+        />
+      }
+      <button onClick={logout}>log out</button>
 
-      </div>
+    </div>
   )
 }
 
