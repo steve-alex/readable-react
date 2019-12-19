dc2dd688e7e56f6276291c5be2c40f8934da6a34
steve-alex <steve.alex@hotmail.co.uk>
2019-12-19 13:42:47 +0000

    Rendering posts on timeline

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -7,18 +7,20 @@ import API from '../adapters/api.js'
 
 const HomeContainer = ( {user, logout} ) => {
   const [timeline, setTimeline] = useState([])
+  const [timelineLoaded, setTimelineLoaded] = useState(false)
   //The search bar here, filters the users that are in the whole database, by username, email address etc.
   ///User.find_by(email: params[:search]) \\ User.find_by(username: params[:search])
   useEffect(() => {
     //Fetch from the the users timeline serializer
     API.getTimeline()
       .then(res => setTimeline(res.timeline))
+      .then(() => setTimelineLoaded(true))
   }, [])
   
   return (
     <div>
       <h1>Readable</h1>
-      {timeline &&
+      {timelineLoaded &&
         <TimelineContainer
           timeline={timeline}
         />
--- src/containers/TimelineContainer.js
+++ src/containers/TimelineContainer.js
@@ -3,21 +3,54 @@ import { Rating } from 'semantic-ui-react'
 
 const TimelineContainer = ( {timeline} ) => {
 
+  const renderComments = (post) => {
+    // console.log(post.comments)
+    // post.comments.map(comment => {
+    //   return (
+    //     <>
+    //       <h4>{comment.user.username}</h4>
+    //     </>
+    //   )
+    // })
+  }
+
   return (
     <>
-      {timeline.timeline_posts &&
+      {
         timeline.timeline_posts.map(post => {
-          return (
-            <>
-              <p>{post.content}</p>
-              <Rating
-                icon="star"
-                defaultRating={post.rating}
-                maxRating={5}
-                disabled/>
-            </>
-          )
-        })
+        return (
+          <>
+            <p>{post.user.username}</p>
+            <Rating
+              icon="star"
+              defaultRating={post.rating}
+              maxRating={5}
+              disabled
+            />
+            <p>{post.created_at.slice(0, 10)}</p>
+            <h2>{post.book.title}</h2>
+            <h5>{post.book.subtitle}</h5>
+            <img src={post.book.image_url}></img>
+            <p>{post.book.description}</p>
+            <p>Page Count: {post.book.page_count}</p>
+            <p>{post.content}</p>
+            <h2>Comments</h2>
+            {/* {post.comments[0] &&
+              renderComments()} */}
+            {post.comments[0] &&
+              post.comments.map(comment => {
+                return (
+                  <>
+                    <h4>{comment.user.username}</h4>
+                    <h4>{comment.created_at.slice(0, 9)}</h4>
+                    <p>{comment.content}</p>
+                  </>
+                )
+              })
+            }
+          </>
+        )
+      })
       }
     </>
   )
