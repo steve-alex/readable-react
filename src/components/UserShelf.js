e3500876615417b1c68b06e868ce53e08587151d
steve-alex <steve.alex@hotmail.co.uk>
2019-12-22 18:47:55 +0000

    Created user Shelf

--- src/components/UserShelf.js
+++ src/components/UserShelf.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import { Redirect } from 'react-router-dom'
 
 const UserShelf = ( {shelfName, shelf} ) => {
+  const [redirect, setRedirect] = useState(undefined)
 
   const renderImages = () => {
     return shelf.books_to_display.map(book => {
-      return <img src={book.image_url}></img>
+      return (
+        <img
+          onClick={() => setRedirect(`/books/${book.book_id}`)}
+          src={book.image_url}></img>
+      )
     })
   }
 
   return (
-    
     <div>
-      <h3>{shelfName}</h3>
       {renderImages()}
-      <h5>Books: {shelf.book_count}</h5>
+      <div onClick={() => setRedirect(`/shelves/${shelf.shelf_id}`)}>
+        <h3>{shelfName}</h3>
+        <h5>Books: {shelf.book_count}</h5>
+      </div>
       <br></br>
+      {redirect &&
+        <Redirect to={redirect}/>
+      }
     </div>
   )
 }
