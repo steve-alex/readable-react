4326e9204b44a7c73254d4d08a1987f9aed7a94a
steve-alex <steve.alex@hotmail.co.uk>
2019-12-22 18:48:05 +0000

    Minor changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
