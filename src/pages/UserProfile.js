d4d29cf902462026e1d42d3e55982baf4c52525b
steve-alex <steve.alex@hotmail.co.uk>
2019-12-31 20:22:10 +0000

    Made 3 new tabs for user page

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import API from '../adapters/api.js'
+import { Menu } from 'semantic-ui-react'
 import UserInformationPanel from '../components/panels/UserInformationPanel.js'
-import { UserShelvesPanel } from '../components/panels/UserShelvesPanel.js'
-import UserFavourites from '../components/UserFavourites.js'
+import { UserShelvesPreviewPanel } from '../components/panels/UserShelvesPreviewPanel.js'
 import { UserGenreMatch } from '../components/panels/UserGenreMatch.js'
 import { UserBooksInCommonPanel } from '../components/panels/UserBooksInCommonPanel.js'
 import { UserShowCurrentlyReadingPanel } from '../components/panels/UserShowCurrentlyReadingPanel.js'
+import { UserShelvesContainer } from '../containers/UserShelvesContainer.js'
+import TimelineContainer from '../containers/TimelineContainer';
 
 const UserPage = ( {match} ) => {
   const [profile, setProfile] = useState(undefined)
+  const [selectedTab, setSelectedTab] = useState('profile')
 
   useEffect(() => {
     API.getUserProfile(match.params.userId)
       .then(resp => setProfile(resp.profile))
   }, [])
+
+  const handleItemClick = (e, {name}) => {
+    setSelectedTab(name)
+  }
   
   return (
     <div>
@@ -23,18 +30,45 @@ const UserPage = ( {match} ) => {
             user={profile.user}
             userFollows={profile.user_follows}
             followObject={profile.follow_object}/>
-          <UserGenreMatch
-            genreMatch={profile.genre_match}/>
-          <UserShelvesPanel
-            shelves={profile.shelves}/>
-          <UserBooksInCommonPanel
-            booksInCommon={profile.books_in_common}/>
-
-          <UserShowCurrentlyReadingPanel
-            userId={profile.user.id}/>
+          <Menu attatched="top" tabular>
+            <Menu.Item
+              name="profile"
+              active={selectedTab === "profile"}
+              onClick={handleItemClick}
+            />
+              <Menu.Item
+              name="posts"
+              active={selectedTab === "posts"}
+              onClick={handleItemClick}
+            />
+            <Menu.Item
+              name="shelves"
+              active={selectedTab === "shelves"}
+              onClick={handleItemClick}
+            />
+          </Menu>
           {/* <UserFavourites
             favouriteGenres={profile.favourite_genres}
             favouriteAuthors={profile.favourite_authors}/> */}
+          {(selectedTab === "profile") &&
+            <div>
+            <UserGenreMatch
+              genreMatch={profile.genre_match}/>
+            <UserShelvesPreviewPanel
+              shelves={profile.shelves}/>
+            <UserBooksInCommonPanel
+              booksInCommon={profile.books_in_common}/>
+            <UserShowCurrentlyReadingPanel
+              userId={profile.user.id}/>
+            </div>}
+          {(selectedTab === "posts") && 
+            <TimelineContainer
+              timeline={profile.posts}
+              commentsHidden={true}/>
+          }
+          {(selectedTab === "shelves") &&
+            <UserShelvesContainer
+              shelves={profile.shelves}/>}
         </div>
       }
     </div>
--- src/pages/UserShelfPage.js
+++ src/pages/UserShelfPage.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect, Link } from 'react-router-dom'
 import API from '../adapters/api.js'
 import './pages.scss'
 
 export const UserShelfPage = ( {user, match} ) => {
   const [shelf, setShelf] = useState("")
+  const [clickedId, setClickedId] = useState(false)
   
   useEffect(() => {
     API.getShelf(match.params.shelfId)
       .then(res => setShelf(res.shelf))
       .catch(console.log)
   }, [])
+
+  const handleClick = (bookId) => {
+    setClickedId(bookId)
+  }
 
   return (
     <div className="userShelfPage">
@@ -19,8 +25,16 @@ export const UserShelfPage = ( {user, match} ) => {
           shelf.books.map(book => {
             return(
               <div class="bookOnShelf">
-                <img src={book.image_url}></img>
-                <h5>{book.title}</h5>
+                <img
+                  onClick={() => handleClick(book.id)}
+                  className="image-hoverable" src={book.image_url}></img>
+                <h5
+                  className="text-hoverable"
+                  onClick={(e) => handleClick(book.id)}>
+                  <span>{book.title}</span></h5>
+                  {clickedId && 
+                    <Redirect to={`books/${clickedId}`} />}
+
               </div>
             )
           })
