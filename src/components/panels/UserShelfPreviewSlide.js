93b95583dc09ce8491dc263ffc0765a2c2b11836
steve-alex <steve.alex@hotmail.co.uk>
2019-12-31 20:21:42 +0000

    Created carousels for all user's shelves

---
+++ src/components/panels/UserShelfPreviewSlide.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import { ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+export const UserShelfPreviewSlide = ( {shelfName, shelf} ) => {
+  const [redirect, setRedirect] = useState(undefined)
+
+  const renderImages = () => {
+    return shelf.books_to_display.slice(0, 4).map(book => {
+      return (
+        <div className="userShelfItem">
+          <img src={book.image_url}></img>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div className="userShelf">
+      {renderImages()}
+      <div className="userShelfName">
+        <div className="userShelfButtons">
+          <h3 className="text-hoverable">
+            <span
+              onClick={() => setRedirect(`/shelves/${shelf.shelf_id}`)}>
+              {shelfName}
+            </span>
+          </h3>
+        </div>
+      </div>
+      {redirect &&
+        <Redirect to={redirect}/>
+      }
+    </div>
+  )
+}
---
+++ src/containers/UserShelvesContainer.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { UserShelfDisplayPanel } from '../components/panels/UserShelfDisplayPanel.js'
+import './containers.scss'
+
+export const UserShelvesContainer = ( {shelves} ) => {
+
+  return(
+    <div className="userShelvesContainer">
+      {shelves &&
+        Object.keys(shelves).map((shelf, index) => {
+          return(
+            <UserShelfDisplayPanel
+              shelfName={shelf}
+              shelfId={shelves[shelf].shelf_id}
+              books={shelves[shelf].books_to_display}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
