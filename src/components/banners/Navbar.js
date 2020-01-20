8eaff860dd5111eeb7ffa4016e086b60b0b5bd43
steve-alex <steve.alex@hotmail.co.uk>
2020-01-20 18:05:15 +0000

    Redesigned Navbar

--- src/components/banners/Navbar.js
+++ src/components/banners/Navbar.js
@@ -1,57 +1,67 @@
-import React, { useState } from 'react'
-import { Menu, Icon } from 'semantic-ui-react'
-import { useHistory, Redirect} from "react-router-dom"
+import React, { useState } from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import { useHistory, Redirect } from "react-router-dom";
+import './banners.scss'
+import logo from './logo.png';
 
-
-export const Navbar = ( {userId, logout} ) => {
-  const [activeItem, setActiveItem] = useState('/home')
+export const Navbar = ({ userId, logout }) => {
+  const [activeItem, setActiveItem] = useState("/home");
   const history = useHistory();
 
-  const handleItemClick = (e, {name}) => {
-    setActiveItem(name)
-    history.push(`/${name}`)
-  }
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+    history.push(`/${name}`);
+  };
 
   return (
-    <Menu
-      size="large"
-      >
-      <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-      >
-        <Icon name="home" />
-        {(activeItem === 'home') && <Redirect to="/home"></Redirect>}
-      </Menu.Item>
-      <Menu.Item
-        name='search'
-        active={activeItem === 'search'}
-        onClick={handleItemClick}>
-        <Icon name="search" />
-        {(activeItem === 'search') && <Redirect to="/search"></Redirect>}
-      </Menu.Item>
-      <Menu.Item
-        name="profile"
-        active={activeItem === `profile`}
-        onClick={handleItemClick}>
-        <Icon name="user outline" />
-      {(activeItem === 'profile') && <Redirect to={`/users/${userId}`}></Redirect>}
-      </Menu.Item>
-      <Menu.Item
-        name='settings'
-        active={activeItem === 'settings'}
-        onClick={handleItemClick}>
-        <Icon name="setting" />
-        {(activeItem === 'settings') && <Redirect to="/settings"></Redirect>}
-      </Menu.Item>
-      {/* <Menu.Item
-        name='logout'
-        active={activeItem === 'logout'}
-        onClick={logout}>
-        Log out
-        {(activeItem === 'logout')}
-      </Menu.Item> */}
-    </Menu>
-  )
-}
+    <div className="navbar">
+      <div className="image">
+        <img alt="increadable-logo" src={logo}></img>
+      </div>
+      <div></div>
+      <Menu size="large" secondary>
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={handleItemClick}
+        >
+          Home
+          {activeItem === "home" && <Redirect to="/home"></Redirect>}
+        </Menu.Item>
+        <Menu.Item
+          name="search"
+          active={activeItem === "search"}
+          onClick={handleItemClick}
+        >
+          Search
+          {activeItem === "search" && <Redirect to="/search"></Redirect>}
+        </Menu.Item>
+        <Menu.Item
+          name="profile"
+          active={activeItem === `profile`}
+          onClick={handleItemClick}
+        >
+          User
+          {activeItem === "profile" && (
+            <Redirect to={`/users/${userId}`}></Redirect>
+          )}
+        </Menu.Item>
+        <Menu.Item
+          name="settings"
+          active={activeItem === "settings"}
+          onClick={handleItemClick}
+        >
+          Settings
+          {activeItem === "settings" && <Redirect to="/settings"></Redirect>}
+        </Menu.Item>
+        {/* <Menu.Item
+          name='logout'
+          active={activeItem === 'logout'}
+          onClick={logout}>
+          Log out
+          {(activeItem === 'logout')}
+        </Menu.Item> */}
+      </Menu>
+    </div>
+  );
+};
